02de917b0f6ffbd290489e293b1d9f7314b1fbd2
kchua <kurtlandchua@yahoo.com>
2016-01-09 14:34:11 -0800

    Fixed issue #1

--- TicTacToe.py
+++ TicTacToe.py
@@ -6,7 +6,7 @@ plyr = {1: 'O', 2: 'X'}
 while True:
     while True:
         try:
-            response = input("Welcome to Tic-Tac-Toe! Please type either 'next-gen' or 'classic'.\n\n")
+            response = input("Welcome to Tic-Tac-Toe! Please type either 'next-gen' or 'classic', without quotes.\n\n")
             assert response == 'next-gen' or response == 'classic'
             break
         except AssertionError:
@@ -37,14 +37,13 @@ while True:
                 try:
                     print("Player " + str(player) + ", please choose a spot.")
                     response = input("Enter a number 0 - 2, followed by another number 0 - 2, separated by a space.\n\n")
-                    assert re.search(r'[0-2] [0-2]', response) is not None
+                    assert re.search(r'[0-2] [0-2]', response) is not None, "Invalid input. Please try again."
+                    x, y = response.split()
+                    x, y = int(x), int(y)
+                    board.change(x, y, plyr[player])
                     break
-                except AssertionError:
-                    print("Invalid input. Please try again.")
-
-            x, y = response.split()
-            x, y = int(x), int(y)
-            board.change(x, y, plyr[player])
+                except AssertionError as e:
+                    print(e)
 
             if board.winner is None:
                 print(board)
@@ -56,12 +55,13 @@ while True:
                             print("Player " + str(player) + ", please choose the board you would like to play on.")
                             response = input("Enter a number 0 - 2, followed by another number 0 - 2, separated by a space.\n\n")
                             assert re.search(r'[0-2] [0-2]', response) is not None
+                            x, y = response.split()
+                            x, y = int(x), int(y)
+                            assert board[board.position].winner is not None, "Board is already won. Please choose another board."
+                            board.position = (x, y)
                             break
                         except AssertionError:
                             print("Invalid input. Please try again.")
-                    x, y = response.split()
-                    x, y = int(x), int(y)
-                    board.position = (x, y)
 
         print(board)
         print("Congratulations, player " + str(player) + "!")

a01643a8d3953c6eb5ca064384a85d7c291ca7c8
kchua <kurtlandchua@yahoo.com>
2016-01-09 16:43:26 -0800

    Fixed infinite loop in user interface

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,5 +1,6 @@
 from Gameboard import *
 import re
+import os
 
 plyr = {1: 'O', 2: 'X'}
 
@@ -13,7 +14,7 @@ while True:
             print(response + 'is not valid input. Please try again.')
 
     if response == 'classic':
-        print("This feature has not yet been implemented. Please come back soon! \n\n")
+        pass
 
     elif response == 'next-gen':
         board, player = Bigboard(), 1
@@ -54,14 +55,26 @@ while True:
                         try:
                             print("Player " + str(player) + ", please choose the board you would like to play on.")
                             response = input("Enter a number 0 - 2, followed by another number 0 - 2, separated by a space.\n\n")
-                            assert re.search(r'[0-2] [0-2]', response) is not None
+                            assert re.search(r'[0-2] [0-2]', response) is not None, "Invalid input. Please try again."
                             x, y = response.split()
                             x, y = int(x), int(y)
-                            assert board[x,y].winner is not None, "Board is already won. Please choose another board."
+                            assert board[x,y].winner is None, "Board is already won. Please choose another board."
                             board.position = (x, y)
                             break
-                        except AssertionError:
-                            print("Invalid input. Please try again.")
+                        except AssertionError as e:
+                            print(e)
 
         print(board)
         print("Congratulations, player " + str(player) + "!")
+
+    while True:
+        try:
+            response = input("Play again? (Y/N)")
+            assert response == 'Y' or response == 'N'
+            break
+        except AssertionError:
+            print("Invalid input. Please try again.")
+
+    if response == 'N':
+        print('Goodbye!')
+        os._exit(-1)
